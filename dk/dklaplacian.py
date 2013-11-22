d95f2b4521c580799206b106cc75551aa0a785f1
Ronojoy Adhikari <ronojoy.adhikari@gmail.com>
2013-11-22 13:03:58 -0500

    Create dklaplacian.py

---
+++ dk/dklaplacian.py
@@ -0,0 +1,10 @@
+''' 
+Isotropic laplacians derived from discrete kinetic theory described in 
+
+Sumesh P. Thampi, Santosh Ansumali, R. Adhikari and Sauro Succi, Isotropic discrete Laplacian operators 
+from lattice hydrodynamics, Journal of Computational Physics, Volume 234, 1 February 2013, Pages 1–7
+
+'''
+import numpy as np
+import scipy as sp
+from scipy import sparse

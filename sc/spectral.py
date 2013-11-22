6ecd32f33c58bfc4a7005ffb61cf09453cced3aa
Ronojoy Adhikari <ronojoy.adhikari@gmail.com>
2013-11-22 11:44:35 -0500

    Update spectral.py

--- sc/spectral.py
+++ sc/spectral.py
@@ -24,36 +24,31 @@ functions :
  
 I. Differentiation Matrices (Polynomial Based)
 
-1.    poldif: General differentiation matrices.
-2.    chebdif: Chebyshev differentiation matrices. 
-3.    herdif: Hermite differentiation matrices. 
-4.    lagdif: Laguerre differentiation matrices.
+1.  poldif: General differentiation matrices.
+2.  chebdif: Chebyshev differentiation matrices. 
+3.  herdif: Hermite differentiation matrices. 
+4.  lagdif: Laguerre differentiation matrices.
 
 II. Differentiation Matrices (Non-Polynomial)
 
-1.   fourdif: Fourier differentiation matrices.  
-2.   sincdif: Sinc differentiation matrices. 
+1.  fourdif: Fourier differentiation matrices.  
+2.  sincdif: Sinc differentiation matrices. 
 
 III. Boundary Conditions
 
-1.   cheb2bc: Chebyshev 2nd derivative matrix
-                incorporating Robin conditions.
-2.   cheb4c: Chebyshev 4th derivative matrix
-                incorporating clamped conditions.
+1.  cheb2bc: Chebyshev 2nd derivative matrix incorporating Robin conditions.
+2.  cheb4c: Chebyshev 4th derivative matrix incorporating clamped conditions.
 
 IV. Interpolation
 
-1.   polint: Barycentric polynomial interpolation on
-                arbitrary distinct nodes
-2.   chebint: Barycentric polynomial interpolation on
-                Chebyshev nodes. 
-3.   fourint: Barycentric trigonometric interpolation at
-                equidistant nodes. 
+1.  polint: Barycentric polynomial interpolation on arbitrary distinct nodes
+2.  chebint: Barycentric polynomial interpolation on Chebyshev nodes. 
+3.  fourint: Barycentric trigonometric interpolation at equidistant nodes. 
 
 V. Transform-based derivatives  
 
-1. chebdifft: Chebyshev derivative
-2. fourdifft: Fourier derivative
+1.  chebdifft: Chebyshev derivative
+2.  fourdifft: Fourier derivative
 3.  sincdift: Sinc derivative 
 
 VI. Roots of Orthogonal Polynomials
@@ -64,20 +59,14 @@ VI. Roots of Orthogonal Polynomials
 
 VII. Examples
 
-1.     cerfa.m: Function file for computing the complementary 
-                error function.  Boundary condition (a) is used.
+1.     cerfa.m: Function file for computing the complementary  error function.  Boundary condition (a) is used.
 2.     cerfb.m: Same as cerfa.m but boundary condition (b) is used.
-3.   matplot.m: Script file for plotting the characteristic curves 
-                of Mathieu's equation.
-4.       ce0.m: Function file for computing the Mathieu cosine
-                elliptic function.
+3.   matplot.m: Script file for plotting the characteristic curves of Mathieu's equation.
+4.       ce0.m: Function file for computing the Mathieu cosine elliptic function.
 5.     sineg.m: Script file for solving the sine-Gordon equation.
-6.     sgrhs.m: Function file for computing the right-hand side of 
-                the sine-Gordon system.
-7.    schrod.m: Script file for computing the eigenvalues of the 
-                Schr\"odinger equation.
-8.    orrsom.m: Script file for computing the eigenvalues of the 
-                Orr-Sommerfeld equation.
+6.     sgrhs.m: Function file for computing the right-hand side of the sine-Gordon system.
+7.    schrod.m: Script file for computing the eigenvalues of the Schr\"odinger equation.
+8.    orrsom.m: Script file for computing the eigenvalues of the Orr-Sommerfeld equation.
 """
 
 import numpy as np

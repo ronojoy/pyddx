db659fa521050681f4f71ea00181e5ca81645692
Ronojoy Adhikari <ronojoy.adhikari@gmail.com>
2013-11-30 00:58:10 -0500

    Implements periodic boundary conditions
    Amit's contribution for periodic boundary code

--- fd/fddif.py
+++ fd/fddif.py
@@ -37,36 +37,72 @@ def fdweights(x0, x, m, dtype='float_'):
 
 def fdmatrix(N, h, ddxorder, stencilwidth):
     '''
-    Constructs sparse central difference matrices of order ddxorder and accuracy ddxaccuracy
-    on a periodic grid of N points with spacing h. 
+    Constructs sparse central difference matrices using stencils 2m+1 points wide on a
+    periodic grid of N points with spacing h. 
     
-    The implementation is limited to stencils that are no larger than 9 points wide. This limits
-    both the order of the derivative (no greater than 8) and the accuracy (no greater than 8th order). 
+    This implementation is limited to stencils that are no larger than 
+    9 points wide. This limits both the order of the derivative 
+    (no greater than 8) and the accuracy (no greater than 8th order). 
     
     The weights are obtained from :
-    B. Fornberg, Generation of Finite Difference Formulas on Arbitrarily Spaced Grids,
-        Mathematics of Computation 51, no. 184 (1988): 699-706.
+    B. Fornberg, Generation of Finite Difference Formulas on Arbitrarily 
+    Spaced Grids,  Mathematics of Computation 51, no. 184 (1988): 699-706.
     '''
     
     if ddxorder > 8 :
         print "please choose a lower order of derivative"
         return 0
         
-    if stencilwidth not in [3, 5, 7, 9] :
+    if stencilwidth not in [3, 5, 7, 9]:
         print "please choose a stencil of width 3, 5, 7 or 9"
         return 0
         
     # the stencil edges
-    stenciledge = (stencilwidth - 1) / 2 
+    stencilhalfwidth = (stencilwidth - 1) / 2 
+    stencilpoints =  np.arange(-stencilhalfwidth, stencilhalfwidth + 1)
     
     # obtain complete array of finite difference weights
-    weights = fdweights(0, np.arange(-stenciledge, stenciledge + 1), ddxorder)
+    weights = fdweights(0, stencilpoints, ddxorder)
     
-    # obtain weights for the requested order of derivative 
-    wdiags = weights[ddxorder,:]
+    # obtain weights for the order of derivative requested
+    w = weights[ddxorder,:]
     
-    # assemble the matrix by assigning the matrix diagonals
-    diags = np.arange(-stenciledge, stenciledge + 1)
+    # assemble the matrix by defining diagonals of correct bandwidth
+    dx = np.ones([stencilwidth, N])    
+    
+    for i in np.arange(stencilwidth):        
+         dx[i, :] = dx[i, :]*w[i]
+        
+    D = sp.sparse.spdiags(dx, stencilpoints, N, N, 'lil') 
+     
+    # for pbc construct upper boundary matrix
+    diags1 = np.arange(N-stencilhalfwidth, N)
+    dx1 = np.ones([stencilhalfwidth, N])    
+     
+    # odd/even derivates are antisymmetric/symmetric  
+    for i in np.arange(stencilhalfwidth): 
+        dx1[i, :] = dx1[i, :]*w[i]            
+           
+    D1 = sp.sparse.spdiags(dx1, diags1, N, N, 'lil') 
+    
+    # for pbc construct lower boundary matrix
+    diags2 = np.arange(-N+stencilhalfwidth, -N,-1)
+    dx2 = np.ones([stencilhalfwidth, N]) 
+    
+    # odd/even derivates are antisymmetric/symmetric 
+    for i in np.arange(stencilhalfwidth): 
+        if ddxorder not in [2, 4, 6, 8]:
+            dx2[i, :] = -dx2[i, :]*w[i]
+        else:
+            dx2[i, :] = dx2[i, :]*w[i]
+    
+    D2 = sp.sparse.spdiags(dx2, diags2, N, N, 'lil') 
+    
+    # assemble all parts
+    DD = (D+D1+D2)/pow(h,ddxorder) 
+    DD = DD.tocsr()
+    
+    return DD
     
     dx = np.ones([stencilwidth, N])    
     

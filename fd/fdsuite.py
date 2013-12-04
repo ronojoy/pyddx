134b793cffa062c3f02021518e3b17e67c45123b
Ronojoy Adhikari <ronojoy.adhikari@gmail.com>
2013-12-04 02:40:31 -0500

    Update fdsuite.py
    Fixed minor error in cdif example code

--- fd/fdsuite.py
+++ fd/fdsuite.py
@@ -191,15 +191,15 @@ def cdif(N, m, stw, h):
     
     The derivatives of functions can be obtained by multiplying the vector of
     function values by the differentiation matrix. The 3-point stencil 
-    approximations of the first two derivatives of a a grid function f can 
-    be obtained as
+    approximations of the first two derivatives of the grid function y = f(x) 
+    can be obtained as
     
     >>> N = 32; h = 2*pi/N; x = np.arange(N) * h
-    >>> f = sin(x)
+    >>> y = sin(x)
     >>> from pyddx.fd import fdsuite as fds
     >>> D1 = fds.cdif(N, 1, 3, h)   # first derivative
     >>> D2 = fds.cdif(N, 2, 3, h)   # second derivative
-    >>> plot(x, y, 'b', x, D1.dot(y), 'r*', x, D2.dot(y), 'r-')
+    >>> plot(x, y, 'r', x, D1.dot(y), 'g', x, D2.dot(y), 'b')
     
     To view the differentiation matrix use 
     
